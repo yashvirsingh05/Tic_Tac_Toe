7f2d34dbe569c77fe9560387e3e8e1f9bd439a7c
yashvir <yashvir.singh_cs18@gla.ac.in>
2020-04-10 02:16:16 -0700

    scanner class create

--- Tic_Tac_Toe4.java
+++ Tic_Tac_Toe4.java
@@ -3,4 +3,8 @@ import java.util.Scanner;
 
 import static java.lang.System.exit;
 public class Tic_Tac_Toe4 {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter cells: ");
+    }
 }
