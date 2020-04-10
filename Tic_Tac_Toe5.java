58bad2a6df4c8782b29e4f9247172e4c5c7f9492
yashvir <yashvir.singh_cs18@gla.ac.in>
2020-04-10 02:28:42 -0700

    create while codition true

--- Tic_Tac_Toe5.java
+++ Tic_Tac_Toe5.java
@@ -4,4 +4,15 @@ import java.util.Scanner;
 
 import static java.lang.System.exit;
 public class Tic_Tac_Toe5 {
+    public static void main(String[] args) {
+        while (true) {
+            int count1 = 0;
+            int count2 = 0;
+            char index = ' ';
+            char variable = 'X';
+            int countElement = 0;
+            Scanner sc = new Scanner(System.in);
+            System.out.println("---------");
+        }
+    }
 }
