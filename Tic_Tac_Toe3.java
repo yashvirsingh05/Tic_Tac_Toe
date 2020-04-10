25817c70880aeb85b1d06475cf72e36b0d006141
yashvir <yashvir.singh_cs18@gla.ac.in>
2020-04-10 02:08:34 -0700

    create loop

--- Tic_Tac_Toe3.java
+++ Tic_Tac_Toe3.java
@@ -10,5 +10,22 @@ public class Tic_Tac_Toe3 {
         System.out.println("---------");
         char[][] newCells = {{cells[0], cells[1], cells[2]}, {cells[3], cells[4], cells[5]}, {cells[6], cells[7], cells[8]}};
         System.out.print("| ");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(newCells[i][j] + " ");
+                if (newCells[i][0] == newCells[i][1] && newCells[i][1] == newCells[i][2]) {
+                    count1 += 1;
+                    if (count1 == 3) {
+                        index = newCells[i][0];
+                    }
+                } else if (newCells[0][i] == newCells[1][i] && newCells[1][i] == newCells[2][i]) {
+                    count2 += 1;
+                    if (count2 == 3) {
+                        index = newCells[0][i];
+                    }
+                }
+            }
 
-    }}
+        }
+    }
+}
