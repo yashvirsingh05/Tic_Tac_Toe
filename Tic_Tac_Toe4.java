d541a34e01afead86b69c840fb457bccc9285707
yashvir <yashvir.singh_cs18@gla.ac.in>
2020-04-10 02:16:57 -0700

    create char type array

--- Tic_Tac_Toe4.java
+++ Tic_Tac_Toe4.java
@@ -6,5 +6,9 @@ public class Tic_Tac_Toe4 {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter cells: ");
+        char[] cells = sc.next().toUpperCase().toCharArray();
+        System.out.println("---------");
+        char[][] newCells = {{cells[0], cells[1], cells[2]}, {cells[3], cells[4], cells[5]}, {cells[6], cells[7], cells[8]}};
+        System.out.print("| ");
     }
 }
