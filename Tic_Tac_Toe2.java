031d7754c13497f91e68c668cde99e5edfaf6e86
yashvir <yashvir.singh_cs18@gla.ac.in>
2020-04-10 01:55:34 -0700

    scanner class object

--- Tic_Tac_Toe2.java
+++ Tic_Tac_Toe2.java
@@ -1,3 +1,7 @@
 import java.util.Scanner;
 public class Tic_Tac_Toe2 {
-}
+    public static void main(String[] args) {
+        // write your code here
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter cells: ");
+}}
