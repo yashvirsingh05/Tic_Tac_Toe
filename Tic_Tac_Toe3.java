fcfd0c776dcd49d0ac3a8025f52e80810b38c4fd
yashvir <yashvir.singh_cs18@gla.ac.in>
2020-04-10 02:04:40 -0700

    scanner class create

--- Tic_Tac_Toe3.java
+++ Tic_Tac_Toe3.java
@@ -1,3 +1,8 @@
 import java.util.Scanner;
 public class Tic_Tac_Toe3 {
-}
+    public static void main(String[] args) {
+        int count1 = 0;
+        int count2 = 0;
+        char index = ' ';
+        Scanner sc = new Scanner(System.in);
+}}
